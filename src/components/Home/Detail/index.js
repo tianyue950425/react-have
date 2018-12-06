17cca0e317b56d0285995472ed848bd13fcc356f
15040552125 <879164245@qq.com>
2018-12-06 19:09:59 +0800

    login

--- src/components/Home/Detail/index.js
+++ src/components/Home/Detail/index.js
@@ -2,17 +2,53 @@ import React,{Component} from "react";
 import css from "./index.module.scss";
 import action from "./action";
 import {connect} from 'react-redux';
+import axios from 'axios'
 class Detail extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	is:'a'
+	  };
+	}
 	render(){
 		return(
 			<div className={css.Detail}>
-				detail
+				{this.state.is.itemDescImages?
+					<ul className={css.list}>
+						{this.state.is.itemDescImages.map(res=>{
+							return 	<li key={res.itemDescImageId}>
+								<img src={res.imageUrl}/>
+							</li>
+							})
+						}
+						{
+							this.state.is.endTime
+						}
+					</ul>
+					:null
+				}
 			</div>
-			)
+		)
 	}
 	componentDidMount(){
 		this.props.myHideNavbar()
 		console.log(this.props.match.params.id)
+		axios({
+			url:`/api2/item/getItemDetail?itemId=${this.props.match.params.id}`,
+			methods:'get',
+			//client-info:appVersion=5.4&platform=wap&sign=dFYeHwJxoxHPazQb5LBJqFh3+AY=&timestamp=1544080701280
+			//client-info:appVersion=5.4&platform=wap&sign=wNRCKAWwXsPJS7XX+r6zvtUANKI=&timestamp=1544081632153
+			headers:{
+				'client-info':'appVersion=5.4&platform=wap&sign=h0RycvddfoVcVKil39RwfFnGIxA=&timestamp=1544083928568'
+			}
+		}).then(res=>{
+			console.log(res.data.data)
+			this.setState({
+				is:res.data.data
+			})
+			console.log(this.state.is)
+		})
 	}
 	componentWillUnmount(){
 		this.props.myShowNavbar()
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,6 +2,13 @@ import React,{Component} from "react";
 import css from "./index.module.scss";
 import {NavLink} from "react-router-dom"
 class Login extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	isShow:false
+	  };
+	}
 	render(){
 		return (
 		<div className={css.box}>
@@ -15,7 +22,10 @@ class Login extends Component{
 				<div>
 					<input name="password" placeholder="请输入密码"/>
 				</div>
-				<button>登录</button>
+				{
+					this.state.isShow?<span>用户名或密码错误</span>:null
+				}
+				<button onClick={this.handleClick.bind(this)}>登录</button>
 			</form>
 			<ul>
 				<li><NavLink to="/register">注册</NavLink></li>
@@ -23,6 +33,9 @@ class Login extends Component{
 			</ul>
 		</div>
 		)
+	}
+	handleClick(){
+
 	}
 }
 export default Login;
